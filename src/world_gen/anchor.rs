d29dbb142cdb9e71cb27677bf9d6d56d6ae576e9
TheDudeFromCI <thedudefromci@gmail.com>
2022-12-20 05:52:22 -0700

    Added force-chunk loading
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

--- src/world_gen/anchor.rs
+++ src/world_gen/anchor.rs
@@ -8,11 +8,10 @@ use crate::prelude::Region;
 /// itself to stay loaded.
 ///
 /// This component relies on the Position component in order to function.
-#[derive(Debug, Clone, Reflect, FromReflect, Component, Default)]
-#[reflect(Component)]
+#[derive(Debug, Clone, Reflect, Component)]
 pub struct ChunkAnchor {
     /// The world that this chunk anchor is pinned to.
-    world: Option<Entity>,
+    world: Entity,
 
     /// The radius, in chunks, that are triggered to load around the chunk
     /// anchor.
@@ -27,51 +26,46 @@ pub struct ChunkAnchor {
     /// range of this anchor.
     max_radius: u32,
 
+    /// The radius around this chunk anchor that are forcefully loaded before
+    /// the next frame if not already loaded.
+    force_radius: i32,
+
     /// The weighted directional value to apply to this anchor.
     ///
     /// This allows for a loading bias to be applied to the chunk loader to
     /// prioritize loading chunks in a specific direction.
     weighted_dir: Vec3,
-
-    /// The last location that the chunk anchor [`iter`] function was called.
-    /// This is to present redundant iterators from being triggered if the
-    /// chunk anchor has not moved.
-    last_center: Option<IVec3>,
 }
 
 impl ChunkAnchor {
     /// Creates a new chunk anchor instance with the given chunk load radius
-    /// (See [`get_radius`]) and the memory storage radius (See
-    /// [`get_max_radius`]).
+    /// (See [`get_radius`]), memory storage radius (See
+    /// [`get_max_radius`]), and force load radius (See [`get_force_radius`]).
     ///
     /// If the max radius is less than the radius, then the chunk load radius
     /// will be used instead for the memory storage radius.
-    pub fn new(world: Entity, radius: u16, mut max_radius: u32) -> Self {
+    pub fn new(world: Entity, radius: u16, mut max_radius: u32, force_radius: i32) -> Self {
         if max_radius < radius as u32 {
             max_radius = radius as u32;
         }
 
         Self {
-            world: Some(world),
+            world,
             radius,
             max_radius,
+            force_radius,
             weighted_dir: Vec3::ZERO,
-            last_center: None,
         }
     }
 
     /// Gets the entity of the world that this chunk anchor is targeting.
-    pub fn get_world(&self) -> Option<Entity> {
+    pub fn get_world(&self) -> Entity {
         self.world
     }
 
-    /// Sets the world that this chunk anchor should target, if any.
-    ///
-    /// Calling this function will clear the chunk loading cache for this
-    /// anchor.
-    pub fn set_world(&mut self, world: Option<Entity>) {
+    /// Sets the world that this chunk anchor should target.
+    pub fn set_world(&mut self, world: Entity) {
         self.world = world;
-        self.last_center = None;
     }
 
     /// Gets the load radius of this chunk anchor.
@@ -92,18 +86,26 @@ impl ChunkAnchor {
         self.max_radius
     }
 
+    /// Gets the force-load radius of this chunk anchor.
+    ///
+    /// The force load radius is an emergency radius value that causes all
+    /// chunks within this radius around the chunk anchor to immediately be
+    /// loaded before the next frame.
+    ///
+    /// This value may be negative to indicate that there is no force radius for
+    /// this chunk anchor.
+    pub fn get_force_radius(&self) -> i32 {
+        self.force_radius
+    }
+
     /// Sets the chunk load radius of this chunk anchor to a new value. (See
     /// [`get_radius`])
     ///
     /// If the new radius is greater than the max radius value, then the max
     /// radius value will be updated to be equal to the new chunk load
     /// radius.
-    ///
-    /// Updating this value will clear the loading cache attached to this
-    /// anchor.
     pub fn set_radius(&mut self, radius: u16) {
         self.radius = radius;
-        self.last_center = None;
 
         if self.max_radius < self.radius as u32 {
             self.max_radius = self.radius as u32;
@@ -123,6 +125,18 @@ impl ChunkAnchor {
         }
     }
 
+    /// Sets the force radius of this chunk anchor.
+    ///
+    /// The force load radius is an emergency radius value that causes all
+    /// chunks within this radius around the chunk anchor to immediately be
+    /// loaded before the next frame.
+    ///
+    /// This value may be negative to indicate that there is no force radius for
+    /// this chunk anchor.
+    pub fn set_force_radius(&mut self, force_radius: i32) {
+        self.force_radius = force_radius;
+    }
+
     /// Updates the directional weighting of chunks to prioritize chunks closer
     /// to the direction of the given weight vector. The priority value is based
     /// off the dot product between the weighted direction vector provided and
@@ -137,28 +151,30 @@ impl ChunkAnchor {
     /// might be to add a weighted direction based off the direction the
     /// camera is facing to prioritize loading chunks that the
     /// player is looking at.
-    ///
-    /// Calling this function will recalculate the priority value of all chunk
-    /// positions the next time the cache is cleared. Calling this function
-    /// has no overhead, and will only take affect the next time the cache
-    /// is recalculated.
     pub fn set_weighted_dir(&mut self, weighted_dir: Vec3) {
         self.weighted_dir = weighted_dir;
     }
 
     /// Creates an iterator over all chunks that should be loaded from this
     /// chunk anchor.
-    ///
-    /// The chunk anchor's current location must be specified.
-    ///
-    /// Note that this function does cause some allocations based on the
-    /// internal sorting of chunk loading order and priority. This function will
-    /// also mark the chunk anchor as up-to-date, and will cause all future
-    /// iterator calls to return empty until the chunk anchor has moved to a
-    /// new chunk.
-    pub fn iter(&mut self, center: IVec3) -> impl Iterator<Item = IVec3> + '_ {
+    pub fn iter(&self, center: IVec3) -> impl Iterator<Item = IVec3> + '_ {
         let radius = self.radius as i32;
         Region::from_points(center - radius, center + radius).into_iter()
+    }
+
+    /// Creates an iterator over all chunks that should be force loaded from
+    /// this chunk anchor.
+    ///
+    /// If this chunk anchor has a force radius set to a negative value, this
+    /// function returns None and does not require any chunks to be force
+    /// loaded.
+    pub fn iter_force(&self, center: IVec3) -> Option<impl Iterator<Item = IVec3> + '_> {
+        let radius = self.force_radius as i32;
+        if radius >= 0 {
+            Some(Region::from_points(center - radius, center + radius).into_iter())
+        } else {
+            None
+        }
     }
 
     /// Gets the chunk priority of the target chunk.
--- src/world_gen/queue.rs
+++ src/world_gen/queue.rs
@@ -4,10 +4,12 @@
 use bevy::prelude::*;
 use bevy::tasks::{AsyncComputeTaskPool, Task};
 use futures_lite::future;
+use itertools::Itertools;
 use ordered_float::OrderedFloat;
+use sort_by_derive::SortBy;
 
 use super::{ChunkAnchor, WorldGeneratorHandler};
-use crate::prelude::VoxelCommands;
+use crate::prelude::{VoxelCommands, VoxelQueryError};
 use crate::storage::{BlockData, VoxelChunk, VoxelStorage, VoxelWorld};
 
 /// This component indicates that the chunk is currently being loaded in an
@@ -23,44 +25,92 @@ pub struct LoadChunkTask<T: BlockData>(#[reflect(ignore)] Task<VoxelStorage<T>>)
 #[component(storage = "SparseSet")]
 pub struct PendingLoadChunkTask;
 
-/// This function will trigger all of the chunks that are requested by chunk
-/// anchors to begin loading.
-pub fn load_chunks_async<T>(
-    mut anchors: Query<(Entity, &Transform, &mut ChunkAnchor)>,
+/// This system triggers all chunk anchors to force load nearby chunks as needed
+/// to satisfy their force_radius values.
+pub fn force_load_chunks<T>(
+    anchors: Query<(&Transform, &ChunkAnchor)>,
+    generators: Query<&WorldGeneratorHandler<T>, With<VoxelWorld>>,
     mut commands: VoxelCommands,
 ) where
     T: BlockData,
 {
-    for (anchor_id, transform, mut anchor) in anchors.iter_mut() {
-        let Some(world_id) = anchor.get_world() else {
-            continue;
-        };
-
-        if !commands.has_world(world_id) {
-            warn!(
-                ?world_id,
-                ?anchor_id,
-                "Chunk anchor points to world that doesn't exist",
-            );
-            anchor.set_world(None);
-            continue;
-        };
-
+    for (transform, anchor) in anchors.iter() {
         #[cfg(feature = "trace")]
-        let profiler_guard = info_span!("world_gen", target = "find_new_chunks").entered();
+        let _profiler_guard = info_span!("world_gen", target = "force_load_chunks").entered();
+
+        let world_id = anchor.get_world();
+        if !commands.has_world(world_id) {
+            continue;
+        };
+
+        let anchor_pos = transform.translation.as_ivec3() >> 4;
+        let Some(chunk_iter) = anchor.iter_force(anchor_pos) else {
+            continue;
+        };
+
+        for chunk_coords in chunk_iter {
+            match commands.find_chunk(world_id, chunk_coords, true) {
+                Ok(_) => continue,
+                Err(VoxelQueryError::ChunkNotFound(..)) => {
+                    let block_data = match generators.get(world_id).ok().map(|g| g.generator()) {
+                        Some(gen) => gen.generate_chunk(chunk_coords),
+                        None => VoxelStorage::<T>::default(),
+                    };
+
+                    commands
+                        .spawn_chunk(world_id, chunk_coords, block_data)
+                        .unwrap();
+
+                    #[cfg(feature = "meshing")]
+                    commands
+                        .remesh_chunk_neighbors(world_id, chunk_coords)
+                        .unwrap();
+                },
+                Err(err) => panic!("Unexpected state: {:?}", err),
+            }
+        }
+    }
+}
+
+/// This function will trigger all of the chunks that are requested by chunk
+/// anchors to begin loading.
+pub fn load_chunks_async(anchors: Query<(&Transform, &ChunkAnchor)>, mut commands: VoxelCommands) {
+    for (transform, anchor) in anchors.iter() {
+        #[cfg(feature = "trace")]
+        let _profiler_guard = info_span!("world_gen", target = "find_new_chunks").entered();
+
+        let world_id = anchor.get_world();
+        if !commands.has_world(world_id) {
+            continue;
+        };
 
         let anchor_pos = transform.translation.as_ivec3() >> 4;
         for chunk_coords in anchor.iter(anchor_pos) {
-            if commands.find_chunk(world_id, chunk_coords).is_err() {
-                commands
-                    .spawn_chunk(world_id, chunk_coords, PendingLoadChunkTask)
-                    .unwrap();
-            };
+            match commands.find_chunk(world_id, chunk_coords, true) {
+                Ok(_) => continue,
+                Err(VoxelQueryError::ChunkNotFound(..)) => {
+                    commands
+                        .spawn_chunk(world_id, chunk_coords, PendingLoadChunkTask)
+                        .unwrap();
+                },
+                Err(err) => panic!("Unexpected state: {:?}", err),
+            }
         }
 
-        #[cfg(feature = "trace")]
-        profiler_guard.exit();
+        break;
     }
+}
+
+/// A temporary storage container for ordering pending chunks based on their
+/// priority level.
+#[derive(SortBy)]
+struct PendingChunkPriority<T> {
+    /// The current voxel query value  that contains the chunk information.
+    query: T,
+
+    /// The priority value of the chunk.
+    #[sort_by]
+    priority: OrderedFloat<f32>,
 }
 
 /// Moves queued chunk loading tasks to an active async chunk loading task.
@@ -84,46 +134,54 @@ pub fn push_chunk_async_queue<T>(
     }
 
     #[cfg(feature = "trace")]
-    let profiler_guard = info_span!("world_gen", target = "find_next_pending_chunk").entered();
+    let profiler_guard = info_span!("world_gen", target = "find_next_pending_chunks").entered();
 
-    let next_chunk = pending_tasks.iter().max_by_key(|q| {
-        let mut priority = f32::NEG_INFINITY;
+    let next_chunks = pending_tasks
+        .iter()
+        .map(|q| {
+            let mut priority = f32::NEG_INFINITY;
 
-        for (transform, anchor) in anchors.iter() {
-            let pos = transform.translation.as_ivec3() >> 4;
-            let anchor_priority = anchor.get_priority(pos, q.1.chunk_coords());
-            priority = f32::max(priority, anchor_priority);
-        }
+            for (transform, anchor) in anchors.iter() {
+                let pos = transform.translation.as_ivec3() >> 4;
+                let anchor_priority = anchor.get_priority(pos, q.1.chunk_coords());
+                priority = f32::max(priority, anchor_priority);
+            }
 
-        OrderedFloat(priority)
-    });
+            PendingChunkPriority {
+                query:    q,
+                priority: OrderedFloat(priority),
+            }
+        })
+        .k_smallest(available_slots as usize)
+        .map(|p| p.query);
 
     #[cfg(feature = "trace")]
     profiler_guard.exit();
 
-    let Some((chunk_id, pending_task)) = next_chunk else {
-        return;
-    };
+    #[cfg(feature = "trace")]
+    let _profiler_guard = info_span!("world_gen", target = "start_async_tasks").entered();
 
-    let world_id = pending_task.world_id();
-    let chunk_coords = pending_task.chunk_coords();
-    match generators.get(world_id).ok().map(|g| g.generator()) {
-        Some(gen) => {
-            let pool = AsyncComputeTaskPool::get();
-            let task = pool.spawn(async move { gen.generate_chunk(chunk_coords) });
-            commands
-                .entity(chunk_id)
-                .remove::<PendingLoadChunkTask>()
-                .insert(LoadChunkTask(task));
-        },
+    for (chunk_id, pending_task) in next_chunks {
+        let world_id = pending_task.world_id();
+        let chunk_coords = pending_task.chunk_coords();
+        match generators.get(world_id).ok().map(|g| g.generator()) {
+            Some(gen) => {
+                let pool = AsyncComputeTaskPool::get();
+                let task = pool.spawn(async move { gen.generate_chunk(chunk_coords) });
+                commands
+                    .entity(chunk_id)
+                    .remove::<PendingLoadChunkTask>()
+                    .insert(LoadChunkTask(task));
+            },
 
-        None => {
-            commands
-                .entity(chunk_id)
-                .remove::<PendingLoadChunkTask>()
-                .insert(VoxelStorage::<T>::default());
-        },
-    };
+            None => {
+                commands
+                    .entity(chunk_id)
+                    .remove::<PendingLoadChunkTask>()
+                    .insert(VoxelStorage::<T>::default());
+            },
+        };
+    }
 }
 
 /// This system takes in all active async chunk loading tasks and, for each one
@@ -133,15 +191,26 @@ pub fn finish_chunk_loading<T: BlockData>(
     mut commands: VoxelCommands,
 ) {
     #[cfg(feature = "trace")]
-    let profiler_guard = info_span!("world_gen", target = "finalize_chunks").entered();
+    let _profiler_guard = info_span!("world_gen", target = "finalize_chunks").entered();
 
     for (mut task, chunk_meta) in load_chunk_tasks.iter_mut() {
+        #[cfg(feature = "trace")]
+        let poll_task_guard = info_span!("world_gen", target = "poll_load_task").entered();
+
         let Some(chunk_data) = future::block_on(future::poll_once(&mut task.0)) else {
             continue;
         };
 
+        #[cfg(feature = "trace")]
+        poll_task_guard.exit();
+
+        #[cfg(feature = "trace")]
+        let _single_prof_guard =
+            info_span!("world_gen", target = "finalize_chunk", world_id = ?chunk_meta.world_id())
+                .entered();
+
         commands
-            .find_chunk(chunk_meta.world_id(), chunk_meta.chunk_coords())
+            .find_chunk(chunk_meta.world_id(), chunk_meta.chunk_coords(), false)
             .unwrap()
             .remove::<LoadChunkTask<T>>()
             .insert(chunk_data);
@@ -151,7 +220,4 @@ pub fn finish_chunk_loading<T: BlockData>(
             .remesh_chunk_neighbors(chunk_meta.world_id(), chunk_meta.chunk_coords())
             .unwrap();
     }
-
-    #[cfg(feature = "trace")]
-    profiler_guard.exit();
 }
